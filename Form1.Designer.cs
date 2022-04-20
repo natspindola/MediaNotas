f05c4cda40c1e0ed18736132537b172513b26c6d
natspindola <natspindola@hotmail.com>
2022-04-20 20:24:02 -0300

    soma das notas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,98 +29,100 @@ namespace MediaNotas
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtNota1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.txtNota2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.txtNota3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.txtNota4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.txtMedia = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.btnOK = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblSituacao = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota1";
             // 
             // txtNota1
             // 
-            this.txtNota1.AutoSize = true;
-            this.txtNota1.Location = new System.Drawing.Point(83, 72);
+            this.txtNota1.Location = new System.Drawing.Point(135, 65);
             this.txtNota1.Name = "txtNota1";
-            this.txtNota1.Size = new System.Drawing.Size(39, 13);
-            this.txtNota1.TabIndex = 0;
-            this.txtNota1.Text = "Nota 1";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(135, 65);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(135, 102);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtNota1.Size = new System.Drawing.Size(100, 20);
+            this.txtNota1.TabIndex = 1;
             // 
             // txtNota2
             // 
-            this.txtNota2.AutoSize = true;
-            this.txtNota2.Location = new System.Drawing.Point(83, 105);
+            this.txtNota2.Location = new System.Drawing.Point(135, 102);
             this.txtNota2.Name = "txtNota2";
-            this.txtNota2.Size = new System.Drawing.Size(39, 13);
-            this.txtNota2.TabIndex = 2;
-            this.txtNota2.Text = "Nota 2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 20);
+            this.txtNota2.TabIndex = 3;
             // 
-            // textBox3
+            // label2
             // 
-            this.textBox3.Location = new System.Drawing.Point(135, 137);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 5;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nota2";
             // 
             // txtNota3
             // 
-            this.txtNota3.AutoSize = true;
-            this.txtNota3.Location = new System.Drawing.Point(83, 140);
+            this.txtNota3.Location = new System.Drawing.Point(135, 137);
             this.txtNota3.Name = "txtNota3";
-            this.txtNota3.Size = new System.Drawing.Size(39, 13);
-            this.txtNota3.TabIndex = 4;
-            this.txtNota3.Text = "Nota 3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 20);
+            this.txtNota3.TabIndex = 5;
             // 
-            // textBox4
+            // label3
             // 
-            this.textBox4.Location = new System.Drawing.Point(135, 175);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 7;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nota3";
             // 
             // txtNota4
             // 
-            this.txtNota4.AutoSize = true;
-            this.txtNota4.Location = new System.Drawing.Point(83, 178);
+            this.txtNota4.Location = new System.Drawing.Point(135, 175);
             this.txtNota4.Name = "txtNota4";
-            this.txtNota4.Size = new System.Drawing.Size(39, 13);
-            this.txtNota4.TabIndex = 6;
-            this.txtNota4.Text = "Nota 4";
+            this.txtNota4.Size = new System.Drawing.Size(100, 20);
+            this.txtNota4.TabIndex = 7;
             // 
-            // textBox5
+            // label4
             // 
-            this.textBox5.Location = new System.Drawing.Point(135, 263);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 9;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nota4";
             // 
             // txtMedia
             // 
-            this.txtMedia.AutoSize = true;
-            this.txtMedia.Location = new System.Drawing.Point(83, 266);
+            this.txtMedia.Location = new System.Drawing.Point(135, 263);
             this.txtMedia.Name = "txtMedia";
-            this.txtMedia.Size = new System.Drawing.Size(36, 13);
-            this.txtMedia.TabIndex = 8;
-            this.txtMedia.Text = "Média";
+            this.txtMedia.Size = new System.Drawing.Size(100, 20);
+            this.txtMedia.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Média";
             // 
             // btnOK
             // 
@@ -132,22 +134,41 @@ namespace MediaNotas
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Situação:";
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Location = new System.Drawing.Point(132, 326);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(0, 13);
+            this.lblSituacao.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(332, 377);
+            this.Controls.Add(this.lblSituacao);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnOK);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.txtMedia);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.txtNota4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.txtNota3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtNota2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Média de notas";
             this.ResumeLayout(false);
@@ -157,17 +178,19 @@ namespace MediaNotas
 
         #endregion
 
-        private System.Windows.Forms.Label txtNota1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label txtNota2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label txtNota3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label txtNota4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label txtMedia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblSituacao;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace MediaNotas
 {
     public partial class Form1 : Form
     {
+        float nota1, nota2, nota3, nota4, media;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +21,13 @@ namespace MediaNotas
 
         private void btnOK_Click(object sender, EventArgs e)
         {
-            
+            nota1 = float.Parse(txtNota1.Text);
+            nota2 = float.Parse(txtNota2.Text);
+            nota3 = float.Parse(txtNota3.Text);
+            nota4 = float.Parse(txtNota4.Text);
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            txtMedia.Text = Convert.ToString(media);
         }
     }
 }
